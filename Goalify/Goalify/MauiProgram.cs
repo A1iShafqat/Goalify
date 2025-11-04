ad3e6feb4fcfc3a1be30c023ea4807858438b3a6
AliShafqat-RI <ali.shafqat@ebitlogix.com>
2025-11-04 17:14:38 +0500

    chore: Removed Unnecessary Files
    MainPage + View Model

--- Goalify/Goalify/MauiProgram.cs
+++ Goalify/Goalify/MauiProgram.cs
@@ -1,4 +1,4 @@
-﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Maui;
 using Goalify.ViewModel;
 using Microsoft.Extensions.Logging;
 
@@ -17,9 +17,6 @@ namespace Goalify
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
-
-            builder.Services.AddSingleton<MainPage>();
-            builder.Services.AddSingleton<MainPageViewModel>();
 
 #if DEBUG
             builder.Logging.AddDebug();
