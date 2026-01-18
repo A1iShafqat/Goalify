07a9a61b884f23140ce6cf0b89def212bb3e2af5
A1iShafqat <alishafqat2010@hotmail.com>
2026-01-18 23:19:04 +0500

    feat: Routes Added

---
+++ Goalify/Goalify.Common/Routes.cs
@@ -0,0 +1,12 @@
+﻿namespace Goalify.Common;
+
+public static class Routes
+{
+    public const string HomePage = nameof(HomePage);
+    public const string ActivityPage = nameof(ActivityPage);
+    public const string AddActivityPage = nameof(AddActivityPage);
+    public const string RoutinePage = nameof(RoutinePage);
+    public const string AddRoutinePage = nameof(AddRoutinePage);
+    public const string TestPage = nameof(TestPage);
+    public const string ProfilePage = nameof(ProfilePage);
+}
